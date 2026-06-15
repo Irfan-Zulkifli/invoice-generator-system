0eb83b8568753c94908bed18b3d29d828915cfd2
panzul <irfanzul1808@gmail.com>
2026-06-15 17:08:17 +0800

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,28 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $users = [
+            [
+                'email' => 'superadmin@gmail.com',
+                'name' => 'Super Admin',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ],
+            [
+                'email' => 'irfanzul1808@gmail.com',
+                'name' => 'Muhammad Irfan',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ],
+
+        ];
+
+        foreach ($users as $user) {
+            $usr = User::updateOrCreate(
+                ['email' => $user['email']],
+                Arr::except($user, ['email'])
+            );
+        }
     }
 }
